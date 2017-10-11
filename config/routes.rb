c3dda0d5b8037097016154e582f2f249ce13dcd4
ChristopherMaierInc <mail@christophermaier.com.au>
2017-10-11 14:35:53 +1000

    Finished Home and Books pages. Lists all books (messy)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root "pages#home"
+
+  get 'home', to: 'pages#home'
+
+  get 'books', to: 'pages#books'
+
+  get 'authors', to: 'pages#authors'
+
+  resources :pages
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
